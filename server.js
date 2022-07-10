c9bd5d07b1ed345cb22f5f07e414ceec4dae1656
vueno <uenovale@gmail.com>
2022-07-10 20:05:57 -0300

    modo cluster agregado

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 require("dotenv").config() 
 const PORT = process.env.PORT || 3000
-const server = require("./src/config/app")
+const cluster = process.env.CLUSTER
+const [, , argumento] = process.argv
 
-server.listen(PORT, ()=>{
-    console.log(`escuchando el puerto =  ${PORT}`)
-})
-server.on("error", error => console.log(`Error en servidor ${error}`))
+if(argumento === cluster) {
+    const cluster = require("cluster")
+    const numCPUs = require("os").cpus().length
+
+    if(cluster.isMaster) {
+        for(let i=0; i<numCPUs; i++) {
+            cluster.fork()
+        }
+    } else {
+        const server = require("./src/config/app")    
+        server.on("error", error => console.log(`Error en servidor ${error}`))
+    }
+} else {
+    const server = require("./src/config/app")    
+    server.listen(PORT, ()=>{
+        console.log(`escuchando el puerto =  ${PORT}`)
+    })
+    server.on("error", error => console.log(`Error en servidor ${error}`))
+}
