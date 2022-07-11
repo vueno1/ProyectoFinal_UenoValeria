0c43bb0c128b479b58139ddad929d355401da279
vueno <uenovale@gmail.com>
2022-07-11 18:07:29 -0300

    comiteado los DELETE

--- src/config/mongoConnect.js
+++
@@ -1,8 +0,0 @@
-const mongoConnect = require ('connect-mongo')
-const config = require ("./config")
-
-const MongoStore = mongoConnect.create({
-    mongoUrl: config.MONGO_URL
-})
-
-module.exports = MongoStore;
