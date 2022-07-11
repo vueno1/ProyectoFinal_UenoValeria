3ed5db672f8c33027458b0487645b3f2bd378b01
vueno <uenovale@gmail.com>
2022-07-11 14:21:42 -0300

    volver a poner la ruta de DAO producto como estaba

--- src/daos/productos/ProductosDaoMongoDB.js
+++ src/daos/productos/ProductosDaoMongoDB.js
@@ -1,4 +1,4 @@
-const ContenedorMongodb = require("../../contenedores/ContenedorMongodb.js")
+const ContenedorMongodb = require("../../contenedores/ContenedorMongodb")
 const Producto = require("../../models/ProductosModel")
 
 module.exports = class ProductosDaoMongoDB extends ContenedorMongodb {
